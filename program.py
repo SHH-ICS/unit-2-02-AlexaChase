5a7cd91de0e2d5f3993ad5ed9ee407b22d23ebd6
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-03-06 20:53:29 +0000

    new changes

--- program.py
+++ program.py
@@ -1,5 +1,5 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-def myFunction(Hello, World):
+def myFunction(Hello):
   print("Hello, World!")
